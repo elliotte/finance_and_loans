135ec2706a17a834be5d999b3a19303fd89d0a20
--get <m_elliott@live.com>
2015-08-23 20:44:47 +0100

    test precompile line add

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,6 +29,7 @@ Draftapp::Application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
+  config.assets.precompile += ['reports.js', 'ledgerShow.js']
   # Generate digests for assets URLs.
   config.assets.digest = true
 
