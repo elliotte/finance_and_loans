2b74225d0fb50342f4515ac8a270d483635ee70a
--get <m_elliott@live.com>
2015-08-26 11:19:26 +0100

    few cleans n rspec models 349 passing 0 fails

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,37 +1,41 @@
 require 'rails_helper'
 
 describe Comment do
+  
+    before do 
+      Report.any_instance.stub(:build_back_end)
+    end
+
   	it { should belong_to :report }
   	it { should respond_to :commenter}
   	it { should respond_to :body}
 
-  describe 'instance methods' do
+    context 'instance methods' do
+      describe 'set_repdate' do
+        it 'sets repdate from report object' do
+          report = FactoryGirl.create(:report)
+          comment = Comment.new(report_id: report.id)
+          comment.set_repdate
+          expect(comment.repdate).to eq(report.current_end)
+        end
+      end
+    end
+    context 'class methods' do
+      before do
+        @report = FactoryGirl.create(:report)
+        5.times{FactoryGirl.create(:comment, report_id: @report.id)}
+      end
+      describe 'export_data' do
+        it 'returns data for all records' do
+          expect(Comment.export_data(:all, @report)).to eq(@report.comments)
+        end
 
-    describe 'set_repdate' do
-      it 'sets repdate from report object' do
-        report = FactoryGirl.create(:report)
-        comment = Comment.new(report_id: report.id)
-        comment.set_repdate
-        expect(comment.repdate).to eq(report.current_end)
+        it 'returns data for current records' do
+          expect(Comment.export_data(:current, @report)).to eq(@report.comments.where(repdate: @report.current_end))
+        end
       end
     end
 
-  end
 
-  describe 'class methods' do
-    before do
-      @report = FactoryGirl.create(:report)
-      5.times{FactoryGirl.create(:comment, report_id: @report.id)}
-    end
-    describe 'export_data' do
-      it 'returns data for all records' do
-        expect(Comment.export_data(:all, @report)).to eq(@report.comments)
-      end
-
-      it 'returns data for current records' do
-        expect(Comment.export_data(:current, @report)).to eq(@report.comments.where(repdate: @report.current_end))
-      end
-    end
-  end
 
 end
--- spec/models/value_spec.rb
+++ spec/models/value_spec.rb
@@ -1,29 +1,35 @@
 require 'rails_helper'
 
-describe Value, "Associations" do
-	it { should belong_to :report } 
-	it { should validate_presence_of :repdate }
-end
+context "Control" do
+	
+	before { Report.any_instance.stub(:build_back_end) }
 
-describe Value, "Scopes" do
-	let(:report_populated) do
-		FactoryGirl.create(:report_with_values_this_month)
+	describe Value, "Associations" do
+		it { should belong_to :report } 
+		it { should validate_presence_of :repdate }
 	end
-	it 'should respond to its tbvalues' do
-		expect(report_populated.values).to respond_to :tbvalues
-	end
-	it 'should respond to its etbvalues' do
-		expect(report_populated.values).to respond_to :etbvalues
-	end
-end
 
-describe Value, "when saved" do
-	it 'should not have a blank ifrstag' do
-		v = Value.create(:amount => 1.00, :repdate => Time.now )
-    	v.ifrstag.should_not be_blank
-	end	
-	it 'should default ifrstag to No-Mapping' do
-		v = Value.create(:amount => 1.00, :repdate => Time.now )
-    	v.ifrstag.should == "No-Mapping"
-	end	
+	describe Value, "Scopes" do
+		let(:report_populated) do
+			FactoryGirl.create(:report_with_values_this_month)
+		end
+		it 'should respond to its tbvalues' do
+			expect(report_populated.values).to respond_to :tbvalues
+		end
+		it 'should respond to its etbvalues' do
+			expect(report_populated.values).to respond_to :etbvalues
+		end
+	end
+
+	describe Value, "when saved" do
+		it 'should not have a blank ifrstag' do
+			v = Value.create(:amount => 1.00, :repdate => Time.now )
+	    	v.ifrstag.should_not be_blank
+		end	
+		it 'should default ifrstag to No-Mapping' do
+			v = Value.create(:amount => 1.00, :repdate => Time.now )
+	    	v.ifrstag.should == "No-Mapping"
+		end	
+	end
+
 end
