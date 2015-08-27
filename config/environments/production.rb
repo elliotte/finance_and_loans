c532061d7f5752e0c5be487e1a481f9782802e1a
--get <m_elliott@live.com>
2015-08-27 08:46:37 +0100

    salesShow prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,7 @@ Draftapp::Application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
-  config.assets.precompile += ['reports.js', 'ledgerShow.js', 'ledgers.js', 'trialBalance.js', 'salesShowjs']
+  config.assets.precompile += ['reports.js', 'ledgerShow.js', 'ledgers.js', 'trialBalance.js', 'salesShow.js']
   # Generate digests for assets URLs.
   config.assets.digest = true
 
