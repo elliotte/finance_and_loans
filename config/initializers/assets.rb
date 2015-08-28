404a91f741ee188ce4ff6b04dbf4809126491ccc
--get <m_elliott@live.com>
2015-08-28 09:11:44 +0100

    mgr.js.erb change

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,1 +1,1 @@
-Rails.application.config.assets.precompile += ['reports.js', 'ledgerShow.js', 'ledgers.js', 'ledger_manager.js','trialBalance.js', 'salesShow.js']
+Rails.application.config.assets.precompile += ['reports.js', 'ledgerShow.js', 'ledgers.js', 'ledgerManager.js.erb','trialBalance.js', 'salesShow.js']
