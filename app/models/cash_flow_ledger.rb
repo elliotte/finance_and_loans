9dcf69b68d8773d4beeab5910869e08c1006f2c2
--get <m_elliott@live.com>
2016-02-10 15:26:56 +0000

    cashflow page extend for new varibles

--- app/models/cash_flow_ledger.rb
+++ app/models/cash_flow_ledger.rb
@@ -16,17 +16,30 @@ class CashFlowLedger < Ledger
 				start_month: list_month_names,
 				cf_length: ["Quarter", "Half-Year", "Full-Year"]
 			},
-			data: {
-				loans: {
-					# percentage
-					rate: 0.01,
-					# months
-					term: 6,
-					amt: 0.00,
-				}				
-			}
 		}
 		assumptions
+	end
+
+	def cf_drivers_data
+		drivers = {
+			loans: {
+				# percentage
+				rate: 0.01,
+				# months
+				term: 6,
+				amt: 0.00
+			},
+			vat: {
+				cost_rate: 0.2,
+				sales_rate: 0.2,
+			},
+			non_director_wages: [],
+			director_wages: [],
+			employer_nics: {
+				# percentage
+				rate: 0.138
+			}
+		}
 	end
 
 private 
--- spec/models/cash_flow_ledger_spec.rb
+++ spec/models/cash_flow_ledger_spec.rb
@@ -17,7 +17,10 @@ describe CashFlowLedger do
       expect(@ledger).to be_a(CashFlowLedger)
     end
      it 'load base assumptions for a cashflow' do
-        expect(@ledger.base_assumptions).to eq ({:format=>{:start_month=>"January", :cf_length=>"Full-Year"}, :select_options=>{:start_month=>["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"], :cf_length=>["Quarter", "Half-Year", "Full-Year"]}, :data=>{:revenue=>[], :costs=>[]}}) 
+        expect(@ledger.base_assumptions).to eq ( {:format=>{:start_month=>"January", :cf_length=>"Full-Year"}, :select_options=>{:start_month=>["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"], :cf_length=>["Quarter", "Half-Year", "Full-Year"]}}) 
+     end
+     it 'loads cf drivers data' do
+        expect(@ledger.cf_drivers_data).to eq ( {:loans=>{:rate=>0.01, :term=>6, :amt=>0.0}, :vat=>{:cost_rate=>0.2, :sales_rate=>0.2}, :non_director_wages=>[], :director_wages=>[], :employer_nics=>{:rate=>0.138}} ) 
      end
   end
 
